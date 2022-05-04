be5e7a205b0db2cc70c1983a0ea223d86c8fb3cd
Cristiane Abreu <bjwmcd@yahoo.com>
2022-05-04 06:19:58 -0400

    update

--- app5/css-grid/src/App.js
+++ app5/css-grid/src/App.js
@@ -1,12 +1,12 @@
 //import CssGrid from './component/CssGrid';
-import CssGrid2 from './component/CssGrid2';
-import './App.css';
+//import CssGrid2 from './component/CssGrid2';
+import GridTemplate from './component/GridTemplate';
 
 function App() {
   return (
     <div className="#">
         
-          <CssGrid2/>
+          <GridTemplate/>
     </div>
   );
 }
